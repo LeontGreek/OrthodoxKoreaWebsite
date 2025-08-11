abd537a17d8e35afe736c20c2f4d83655f052cc7
LeontG <leontg@proton.me>
2025-08-11 18:31:06 +0900

    Initial Commit

---
+++ Orthodox Korea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orthodox Korea
+//
+//  Created by LeontG Music on 11/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orthodox Korea/Orthodox_KoreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orthodox_KoreaApp.swift
+//  Orthodox Korea
+//
+//  Created by LeontG Music on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Orthodox_KoreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
