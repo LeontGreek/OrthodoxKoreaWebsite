b6a65302398bb2fc2235d3bd4b1ce58f39146ce2
LeontG <leontg@proton.me>
2025-08-12 14:40:34 +0900

    Optimized code

--- Orthodox Korea/WebView.swift
+++ Orthodox Korea/WebView.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
+
+import WebKit
+
+struct Webview : UIViewRepresentable {
+
+
+    let request: URLRequest
+    var webview: WKWebView?
+
+    init(web: WKWebView?, req: URLRequest) {
+        self.webview = WKWebView()
+        self.request = req
+    }
+
+    func makeUIView(context: Context) -> WKWebView  {
+        return webview!
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+
+    func goBack(){
+        webview?.goBack()
+    }
+
+    func goForward(){
+        webview?.goForward()
+    }
+    
+    func reload(){
+        webview?.reload()
+    }
+  }
